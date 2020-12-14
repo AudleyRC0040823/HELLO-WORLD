7e9411c53ede006253d2b8649b49138b4c9e64bf
Ellis Audley <RC0040823@student.riversidecollege.ac.uk>
2020-12-14 15:21:05 +0000

    Added button 2 and textbox 2

--- HELLO WORLD/Form1.Designer.cs
+++ HELLO WORLD/Form1.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.txtBxName = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(488, 264);
+            this.button1.Location = new System.Drawing.Point(532, 198);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 59);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Hello";
+            this.button1.Text = "Goodbye";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
@@ -48,14 +50,33 @@
             this.txtBxName.Name = "txtBxName";
             this.txtBxName.Size = new System.Drawing.Size(137, 20);
             this.txtBxName.TabIndex = 1;
-            this.txtBxName.Text = "Welcome";
+            this.txtBxName.Text = "Hello Ellis";
             this.txtBxName.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(375, 180);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Hello Audley";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 59);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Welcome to Michaels program";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.txtBxName);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -70,6 +91,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtBxName;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
